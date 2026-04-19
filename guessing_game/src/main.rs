d41a1f8aca1d291e41ab176bd19d46cc35c89c97
mwizzard <merlinsuter@outlook.com>
2026-04-19 01:39:08 -0400

    Add Chapter 1 and 2

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,38 @@
+use std::io; // input/output lib
+use rand::Rng; //random lib
+
+use std::cmp::Ordering;
+
+
+fn main() {
+    println!("Guess the number");
+
+    let secret_num = rand::thread_rng().gen_range(1..=100);
+
+    loop{
+        println!("Please input your guess:");
+
+        let mut guess = String::new(); //Create a new mutable string
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,         //is a number
+            Err(_) => continue,     //is not a number
+        };
+
+        println!("You guessed: {guess}");
+        
+        match guess.cmp(&secret_num){
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+
+    }
+}
